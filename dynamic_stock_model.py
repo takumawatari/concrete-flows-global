f3da790afa851b9a3e6b4abac0cf8c6a971d9f8d
Takuma Watari <104181103+takumawatari@users.noreply.github.com>
2023-02-03 14:02:05 +0000

    basic model

---
+++ dynamic_stock_model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.stats import norm
+
+def compute_outflow(inflow, lifetime, sd):
+    """
+    Computes the outflow based on the inflow, lifetime and standard deviation.
+    """
+    # Initialize variables
+    year_complete = np.arange(1900, 1901)
+    outflow = np.repeat(0, len(year_complete))
+
+    # Iterate over years
+    for k in range(1901, 2051):
+        # Compute outflow for each year
+        outflow_list = inflow.iloc[0:len(year_complete)] * norm.pdf(k - year_complete, lifetime.iloc[0:len(year_complete)],  sd.iloc[0:len(year_complete)])
+
+        # Sum up outflow for the year
+        outflow_sum = np.sum(outflow_list)
+
+        # Append the outflow sum to the outflow list
+        outflow = np.append(outflow, outflow_sum)
+
+        # Append the current year to the year_complete list
+        year_complete = np.append(year_complete, k)
+
+    # Convert the outflow list to a pandas Series and return
+    return pd.Series(outflow)
+
+def compute_stock(inflow, outflow):
+    """
+    Computes the stock based on the inflow and outflow.
+    """
+    stock = inflow.sub(outflow).cumsum()
+    return stock
